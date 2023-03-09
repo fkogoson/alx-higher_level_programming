92efc4f7465d9e5e270bddb89b39634c3495e07e
Festus Kogo <fkogoson@gmail.com>
2023-03-08 23:25:49 -0800

    Add 7-edges.py

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str1 = "Holberton"
+print(f"First 3 letters: {str1[0:3]}") # => Hol
+print(f"Last 2 letters: {str1[7:]}") # => on
+print(f"Middle word: {str1[1:8]}") # => olberto
